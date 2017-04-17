0692c80fec6d3f7ad49eecf4d500502754609a99
Susana <girasolespolavida@gmail.com>
2017-04-17 13:37:50 -0300

    cambio dde algunas comillas por tildes al revés (no sé como se llama eso, pero es para incluir las variables dentro de la cadena)

--- main.js
+++ main.js
@@ -1,43 +1,45 @@
+let totalCuadritos = 100
+
 // Constructor que crea cuadraditos
+
 class Cuadrado {
   constructor(id){
     this.box = document.createElement('div')
     this.box.classList.add('sqr')
-    this.box.setAttribute('id','x'+id)
+    this.box.setAttribute('id',`x${id}`)
     return this.box
   } 
 }
 
+
 // Función autoinvocada para llenar el tablero de cuadraditos
+
 (llenarTablero = (cantidadCuadritos) => {
   let tablero = document.getElementById('tablero')
   for(let i = 0; i < cantidadCuadritos; i++){
     let cuadrito = new Cuadrado(i)
     tablero.appendChild(cuadrito)
   }
-})(100);
+})(totalCuadritos);
 
-// Acá asignamos el cuadrito donde comienza a caminar el robot. 
-// El número se asigna de manera aleatoria.
-let randomId = (Math.floor(Math.random() * 100) + 1).toString();
-console.log(randomId)
-document.getElementById('x'+randomId).classList.add('robot')
+// Acá asignamos el cuadrito donde comienza a caminar el robot.El número se asigna de manera aleatoria.
+let randomId = (Math.floor(Math.random() * totalCuadritos) + 1).toString()
+document.getElementById(`x${randomId}`).classList.add('robot')
 
 // Función que mueve al robot con las flechas del teclado
 document.onkeydown = function(ev) {
   let rob = document.getElementsByClassName('robot')[0] // El robot
-    
   // Función para mover el robot arriba y abajo
   const upDown = donde => {
-    let idRobot = rob.getAttribute('id').split("")
+    let idRobot = rob.getAttribute('id').split('')
     idRobot.shift()
     let numeroIdRobot = idRobot.toString()
     let numeritoo = parseInt(numeroIdRobot.charAt(0)+numeroIdRobot.charAt(2))
     let elementoArobotear;
     if(donde=='up'){
-      elementoArobotear = document.getElementById('x'+(numeritoo-10))
+      elementoArobotear = document.getElementById(`x${numeritoo-10}`)
     } else {
-      elementoArobotear = document.getElementById('x'+(numeritoo+10))    
+      elementoArobotear = document.getElementById(`x${numeritoo+10}`)    
     }
     rob.classList.remove('robot')
     elementoArobotear.classList.add('robot')
